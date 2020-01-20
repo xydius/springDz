b4dba2be2c5ba7dc86cbfde402ede2b216883f08
aaraiza <anaiza.araiza@autozone.com>
2020-01-20 15:07:29 -0700

    Initial commit

---
+++ src/main/java/com/dz/datazone/DatazoneApplication.java
@@ -0,0 +1,13 @@
+package com.dz.datazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatazoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatazoneApplication.class, args);
+	}
+
+}

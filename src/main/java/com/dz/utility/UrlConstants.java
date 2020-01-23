0feeb3ce6d88e41ee54043f9c56498508b7608da
aaraiza <anaiza.araiza@autozone.com>
2020-01-23 15:35:00 -0700

    dos

--- src/main/java/com/dz/utility/UrlConstants.java
+++ src/main/java/com/dz/utility/UrlConstants.java
@@ -2,6 +2,10 @@ package com.dz.utility;
 
 public class UrlConstants {
     public static final String INDEX = "";
-    public static final String CUSTOMER = "/customer";
+    public static final String CLIENTE = "/cliente";
+    public static final String CLIENTE_AGREGAR = "/agregar";
+    public static final String CLIENTE_ACTUALIZAR = "/actualizar";
+    public static final String CLIENTE_ELIMINAR = "/eliminar";
+    public static final String CLIENTE_LISTAR = "/listar";
     public static final String CHARLIE = "/charlie";
 }
